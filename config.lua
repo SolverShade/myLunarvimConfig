55238c2b178be5c8b58551697dfe333e2c467523
solvershade <seconduacc07@gmail.com>
2024-11-02 21:08:25 -0400

    added debugging for node.

--- config.lua
+++ config.lua
@@ -3,13 +3,17 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-
+--
 lvim.plugins = {
   { "ellisonleao/gruvbox.nvim",         priority = 1000, config = true, opts = ... },
   { 'mfussenegger/nvim-dap-python' },
   { 'jbyuki/one-small-step-for-vimkind' },
   { 'davepinto/virtual-column.nvim' },
   { 'github/copilot.vim' },
+  { 'adelarsq/image_preview.nvim' },
+  { 'nvim-treesitter/nvim-treesitter' },
+  { "ChristianChiarulli/swenv.nvim" },
+  { "stevearc/dressing.nvim" },
   {
     "christoomey/vim-tmux-navigator",
     cmd = {
@@ -40,6 +44,48 @@ lvim.plugins = {
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
+  {
+    "microsoft/vscode-js-debug",
+    -- After install, build it and rename the dist directory to out
+    build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+    version = "1.*",
+  },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+    config = function()
+      require("dap-vscode-js").setup({
+        -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+        -- node_path = "node",
+
+        -- Path to vscode-js-debug installation.
+        -- debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+        debugger_path = vim.fn.resolve(
+          "/home/powerbox/.local/share/lunarvim/site/pack/lazy/opt/vscode-js-debug"),
+
+        -- Command to use to launch the debug server. Takes precedence over "node_path" and "debugger_path"
+        -- debugger_cmd = { "js-debug-adapter" },
+
+        -- which adapters to register in nvim-dap
+        adapters = {
+          "chrome",
+          "pwa-node",
+          "pwa-chrome",
+          "pwa-msedge",
+          "pwa-extensionHost",
+          "node-terminal",
+        },
+
+        -- Path for file logging
+        -- log_file_path = "(stdpath cache)/dap_vscode_js.log",
+
+        -- Logging level for output to file. Set to false to disable logging.
+        -- log_file_level = false,
+
+        -- Logging level for output to console. Set to false to disable console output.
+        -- log_console_level = vim.log.levels.ERROR,
+      })
+    end,
+  }
 }
 
 
@@ -52,6 +98,8 @@ lvim.keys.normal_mode["d$"] = '"_d$'
 lvim.keys.visual_mode["d$"] = '"_d$'
 lvim.keys.normal_mode["dw"] = '"_dw'
 lvim.keys.visual_mode["dw"] = '"_dw'
+lvim.keys.normal_mode["x"] = '"_x'
+lvim.keys.visual_mode["x"] = '"_x'
 
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
 
@@ -140,6 +188,9 @@ dap.configurations.cs = {
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
+      --return vim.fn.input('Path to dll ', vim.fn.getcwd() .. '/', 'file')
+      --end
+
       if vim.fn.fnamemodify(vim.fn.getcwd(), ":t") == 'build' then
         vim.api.nvim_command('cd .. ')
       end
@@ -196,6 +247,69 @@ require('lspconfig').clangd.setup {
 
 require("dap-python").setup("python")
 
+local js_based_languages = {
+  "typescript",
+  "javascript",
+  "typescriptreact",
+  "javascriptreact",
+  "vue",
+}
+
+for _, language in ipairs(js_based_languages) do
+  dap.configurations[language] = {
+    -- Debug single nodejs files
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+    },
+    -- Debug nodejs processes (make sure to add --inspect when you run the process)
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require("dap.utils").pick_process,
+      cwd = vim.fn.getcwd(),
+      sourceMaps = true,
+    },
+    -- Debug web applications (client side)
+    {
+      type = "pwa-chrome",
+      request = "launch",
+      name = "Launch & Debug Chrome",
+      url = function()
+        local co = coroutine.running()
+        return coroutine.create(function()
+          vim.ui.input({
+            prompt = "Enter URL: ",
+            default = "http://localhost:3000",
+          }, function(url)
+            if url == nil or url == "" then
+              return
+            else
+              coroutine.resume(co, url)
+            end
+          end)
+        end)
+      end,
+      webRoot = vim.fn.getcwd(),
+      protocol = "inspector",
+      sourceMaps = true,
+      userDataDir = false,
+      runtimeExecutable = "/usr/bin/google-chrome", -- Add this line with the path to your Chrome executable
+    },
+    -- Divider for the launch.json derived configs
+    {
+      name = "----- ↓ launch.json configs ↓ -----",
+      type = "",
+      request = "launch",
+    },
+  }
+end
+
 --fix line endings from windows to linux paste
 function Trim()
   local save = vim.fn.winsaveview()
@@ -219,5 +333,26 @@ end
 lvim.keys.normal_mode["p"] = ":lua Paste_and_trim()<CR>"
 vim.api.nvim_create_user_command('Trim', function() Trim() end, {})
 
+--python linting env stuff
+lvim.builtin.treesitter.ensure_installed = {
+  "python",
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup { { name = "black" } }
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup { { command = "flake8", args = { "--ignore=E203" }, filetypes = { "python" } } }
+
+require('swenv').setup({
+  post_set_venv = function()
+    vim.cmd("LspRestart")
+  end,
+})
+
+vim.api.nvim_set_keymap('i', '<C-Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true })
+vim.api.nvim_set_keymap('i', '<C>', 'pumvisible() ? "\\<C-n>" : "\\<C-Space>"', { expr = true })
+
+
 --formats
---lvim.format_on_save = true
+lvim.format_on_save = true
